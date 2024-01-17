b64145f1f2d8d5eb30b0c932c7c6d23c895c152d
Alexandr <Rem271@mail.ru>
2024-01-17 20:28:45 +0300

    5.0. Локальные переменные и поля. [#180148]

--- src/main/java/ru/job4j/oop/User.java
+++ src/main/java/ru/job4j/oop/User.java
@@ -1,11 +1,17 @@
 package ru.job4j.oop;
 
 public class User {
-    private String name; // это поля обьекта т.к  объявляется в области класса
+    /*
+   name, age это поля обьекта т.к  объявляется в области класса
+     */
+    private String name;
     private int age;
 
     public boolean canDrive() {
-        boolean can = false; // это локальная переменная объявляется внутри метода
+        /*
+       can это локальная переменная объявляется внутри метода
+         */
+        boolean can = false;
         if (age >= 18) {
             can = true;
         }
