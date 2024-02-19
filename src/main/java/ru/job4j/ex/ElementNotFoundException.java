ae29f0937e16e4fa5284bf9a5c7dcbba748972e3
Alexandr <Rem271@mail.ru>
2024-02-19 17:08:59 +0300

    0.4. Пользовательские исключения. [#219367 #481963]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -1,4 +1,9 @@
 package ru.job4j.ex;
 
 public class ElementNotFoundException extends Exception {
+    private String message;
+
+    public ElementNotFoundException(String message) {
+        this.message = message;
+    }
 }
