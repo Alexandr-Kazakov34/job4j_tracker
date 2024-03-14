ccabc610dce2ea60df94483d4dd7e416875b882d
Alexandr <Rem271@mail.ru>
2024-03-14 09:22:14 +0300

    0. Map, HashMap [#212652 #485952]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -8,10 +8,15 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("Rem@.mail", "Kazakov A.S");
+        map.put("Rem@.mail", "Sidorov V.V");
+        map.put("Ikon@.ail", "Kazakov A.S");
+        map.put("Non@maill", "Petrov A.S");
+
         for (String s : map.keySet()) {
             String readLine = map.get(s);
             System.out.println(readLine);
         }
+
         for (Map.Entry<String, String> m : map.entrySet()) {
             System.out.println(m);
         }
