cf3b9039a21b766fb48765167f0e215328822c3a
Alexandr <Rem271@mail.ru>
2024-04-01 09:02:12 +0300

    5. PriorityQueue [#504944]

---
+++ src/main/java/ru/job4j/queue/PriorityQueueRunner.java
@@ -0,0 +1,26 @@
+package ru.job4j.queue;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueRunner {
+    public static void main(String[] args) {
+        PriorityQueue<String> stringQueue = new PriorityQueue<>();
+        stringQueue.offer("f");
+        stringQueue.offer("a");
+        stringQueue.offer("s");
+        stringQueue.offer("e");
+        printStringElements(stringQueue);
+    }
+
+    private static void printStringElements(PriorityQueue<String> queue) {
+        while (!queue.isEmpty()) {
+            System.out.println("Current element: " + queue.poll());
+        }
+    }
+
+    private static void printTransactionElements(PriorityQueue<Transaction> queue) {
+        while (!queue.isEmpty()) {
+            System.out.println("Current element: " + queue.poll());
+        }
+    }
+}
