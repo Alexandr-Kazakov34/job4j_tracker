e19f0f28ec5d14897ef3d8eeac92276c1f9f9502
Alexandr <Rem271@mail.ru>
2024-04-08 11:38:39 +0300

    2.0. Лямбда  [#58165 #490278]

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -1,7 +1,9 @@
 package ru.job4j.lambda;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Stack;
 
 public class FI {
     public static void main(String[] args) {
@@ -13,7 +15,7 @@ public class FI {
         Comparator<Attachment> comparator = (left, right) -> Integer.compare(left.getSize(), right.getSize());
         Arrays.sort(attachments, comparator);
 
-        Comparator<String> comparator2 = (left, right) -> Integer.compare(left.length(), right.length());
-        Comparator<String> comparator3 = (left, right) -> Integer.compare(right.length(), left.length());
+        Comparator<String> comparator2 = (left, right) -> left.compareTo(right);
+        Comparator<String> comparator3 = (left, right) -> right.compareTo(left);
     }
 }
